d36afa728f927a6222487239828e31e5e9d8ec66
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2021-07-31 16:12:21 +0900

    docs: add example for AWS Elasticache for Memcached

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster
 resource "aws_rds_cluster" "aurora-postgresql" {
   engine         = "aurora-postgresql"
   engine_version = "11.9" # renovate: depName=suzuki-shunsuke/aws-rds-aurora-postgresql
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster
+resource "aws_elasticache_cluster" "memcached" {
+  engine         = "memcached"
+  engine_version = "1.4.5" # renovate: depName=suzuki-shunsuke/aws-elasticache-memcached
 }
